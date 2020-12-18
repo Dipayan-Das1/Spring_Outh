e2bd6aa2bdfd1e737d4b25fe0e3d1ed4864aeacb
Dipayan-Das1 <dipayan1.das@gmai.com>
2020-12-18 11:54:29 +0530

    Java code

---
+++ src/main/java/edu/spring/oauth/webapp/WebAppApplication.java
@@ -0,0 +1,13 @@
+package edu.spring.oauth.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppApplication.class, args);
+	}
+
+}
